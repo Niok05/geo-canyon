61476c9a1ef179cfbc9f544035056e4842d95e52
Niok05 <cuel.nicolas@gmail.com>
2025-01-18 12:16:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+mapboxgl.accessToken = 'VOTRE_CLE_API'; // Remplacez par votre clé API Mapbox
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [2.3522, 48.8566], // Coordonnées de Paris
+    zoom: 10
+});
